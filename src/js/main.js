5ed0a3f11f004f67ace56dd4b286b18b55f6e99f
Mark Pallone <mark.c.pallone@gmail.com>
2017-05-07 09:42:05 -0400

    Add search box that populates side list

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,48 @@
 var map;
 
-var washingtonDcLatLng = {lat: 38.8934237, lng: -77.0588182};
-
-var sideMenuIsHidden = false;
+var washingtonDcLatLng = {lat: 38.8976755, lng: -77.0451128};
 
 function initMap() {
-
     map = new google.maps.Map(document.getElementById('map'), {
         center: washingtonDcLatLng,
         zoom: 14
     });
 
     /*
-    var locations = [
-        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}}
-    ];
+     * Code taken from https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+     */
 
-    var tribeca = {lat: 40.719526, lng: -74.0089934};
-    var marker = new google.maps.Marker({
-     position: tribeca,
-     map: map,
-     title: 'First Marker!'
-    });
-    var infowindow = new google.maps.InfoWindow({
-     content: 'info window content'
-    });
-    marker.addListener('click', function() {
-     infowindow.open(map, marker);
-    });
-    */
+    var input = document.getElementById('pac-input')
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+        // for (var i = 0; i < places.length; ++i)
+        //     console.log(i, places[i]);
+        viewModel.setListItems(places);
+    });
 }
 
-// todo remove
-for (var i = 0; i < 200; ++i) {
-    $('#test-list').append('<li>Element ' + i + '</li>')
+var ViewModel = function() {
+    var self = this;
+    self.listItems = ko.observableArray([]);
+
+    self.setListItems = function(newListItems) { // todo - should this be ko.computed()?
+        console.log("setListItems() was called with ", newListItems.length, "elements.")
+        // todo - handle the case where newListItems is empty
+        self.listItems.removeAll();
+        for (var i = 0; i < newListItems.length; ++i) {
+            self.listItems.push(newListItems[i]);
+        }
+    }
 }
+
+var viewModel = new ViewModel();
+ko.applyBindings(viewModel);
 
