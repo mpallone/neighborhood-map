5d19e418d852bbb4e423998c5b87bf489d75248f
Mark Pallone <mark.c.pallone@gmail.com>
2017-05-06 21:17:43 -0400

    Add hamburger toggle-able side navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,81 @@
-console.log("Hello!");
-
 var map;
 
 var washingtonDcLatLng = {lat: 38.8934237, lng: -77.0588182};
 
+var sideMenuIsHidden = false;
+
 function initMap() {
+
+    console.log("initMap was called");
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: washingtonDcLatLng,
         zoom: 13
     });
 
-    // var locations = [
-    //     {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-    //     {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}}
-    // ];
-  
-    // var tribeca = {lat: 40.719526, lng: -74.0089934};
-    // var marker = new google.maps.Marker({
-    //  position: tribeca,
-    //  map: map,
-    //  title: 'First Marker!'
-    // });
-    // var infowindow = new google.maps.InfoWindow({
-    //  content: 'info window content'
-    // });
-    // marker.addListener('click', function() {
-    //  infowindow.open(map, marker);
-    // });
+    /*
+    var locations = [
+        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}}
+    ];
+
+    var tribeca = {lat: 40.719526, lng: -74.0089934};
+    var marker = new google.maps.Marker({
+     position: tribeca,
+     map: map,
+     title: 'First Marker!'
+    });
+    var infowindow = new google.maps.InfoWindow({
+     content: 'info window content'
+    });
+    marker.addListener('click', function() {
+     infowindow.open(map, marker);
+    });
+    */
+
 }
+
+
+
+// function toggleSideMenu()
+// {
+//     // todo - should I be using Knockout for this?
+
+//     console.log("-----------------------");
+
+//     if (! sideMenuIsHidden)
+//     {
+
+//     }
+//     else
+//     {
+
+//     }
+
+//     sideMenuIsHidden = !sideMenuIsHidden;
+
+//     if (sideMenuIsHidden) {
+//         console.log("Side menu should now be hidden");
+//     } else {
+//         console.log("Side menu should now be visible");
+//     }
+
+
+// }
+
+// // todo - should I be using knockout for this?
+// $('#hamburger').click(toggleSideMenu);
+
+var slideout = new Slideout({
+  'panel': document.getElementById('map-container'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70
+});
+slideout.toggle(); // start visible
+
+// Toggle button
+document.querySelector('.toggle-button').addEventListener('click', function() {
+  slideout.toggle();
+});
+
